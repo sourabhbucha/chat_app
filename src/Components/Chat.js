b967393f34f00087884601cd5c02fb2884781eef
Sourabh Bucha <sourabhking14@gmail.com>
2021-08-08 15:10:37 +0530

    DatabaseConnection

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase';
 import SignOut from './SignOut'
 
 function Chat() {
+    const [messages,setMessages] = useState([]);
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    },[messages])
     return (
         <div>
             <SignOut />
-            Chat
+            {messages.map(({id, text, photoURL})=>(
+                <div key={id}>
+                    <img src={photoURL} alt="" />
+                    <p>{text}</p>
+                </div>
+            ))}
         </div>
     )
 }
